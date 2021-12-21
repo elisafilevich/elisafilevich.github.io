8db0c04598d1241cf68ec9bf64afcb234fc36b03
Elisa Filevich <elisa.filevich@gmail.com>
2021-12-21 13:09:43 +0100

    try to get the publications to fill in dynamically)

---
+++ allArticles.js
@@ -0,0 +1,29 @@
+var allArticles = [
+    {
+        id: 2,
+        title: '222What is the human sense of agency, and is it metacognitive?',
+        authors: 'Chambon, V. Filevich, E. Haggard, P.',
+        journal: 'In Stephen M. Fleming and Chris Frith (Eds). The cognitive neuroscience of metacognition. Springer (Book chapter)',
+        year: '2014',
+        link_journal: 'https://www.sciencedirect.com/science/article/abs/pii/S0149763412000152'
+    },
+    {
+        id: 1,
+        title: '111Intentional inhibition in human action: The power of “no.”',
+        authors: 'Filevich, E.*, Kühn, S.*, Haggard, P.',
+        journal: 'Neuroscience and Biobehavioral Reviews, 36(4), 1107–1118',
+        year: '2012',
+        link_journal: 'https://www.sciencedirect.com/science/article/abs/pii/S0149763412000152'
+    }]
+
+
+// {id: 2,
+//     title: ,
+//     authors: ,
+//     journal: ,
+//     year: ,
+//     link_journal: ,
+//     link-pdf: ,
+//     link-supplement: ,
+//     link-preregistration
+// }
