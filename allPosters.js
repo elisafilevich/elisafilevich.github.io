e0f4a792e7b9f8749aa3f3cd2690047227d988ce
Elisa Filevich <elisa.filevich@gmail.com>
2022-01-14 00:42:12 +0100

    added posters, updated everybodys profiles. adding the list.js css modified the top-gree-thing format so I just added it everywhere to have consistent formatting. looks nice(r)-ish

---
+++ allPosters.js
@@ -0,0 +1,50 @@
+var allPosters = [
+    {
+        id: 5,
+        title: 'Do we rely on the outcome of our movements to know how we just moved?',
+        authors: 'Charalampaki, A., Filevich, E.',
+        journal: 'Mind, Brain, Body Symposium, MindBrainBody Institute.',
+        year: '2021',
+        link_journal: 'pdfs/A15_Charalampaki_MBB2021.pdf'
+    },
+    {
+        id: 5,
+        title: 'Metacognition of motor control – do we have conscious access to the details of our movements?',
+        authors: 'Peters, C., Filevich, E.',
+        journal: ' ASSC 23, London, Ontario',
+        year: '2019',
+        link_journal: 'pdfs/Peters_Poster_ASSC.pdf'
+    },
+    {
+        id: 4,
+        title: 'Are Agency judgments metacognitive?',
+        authors: 'Constant, M., Filevich, E.',
+        journal: ' Mind, Brain, Body Symposium, MindBrainBody Institute',
+        year: '2021',
+        link_journal: 'pdfs/D10_Constant.pdf'
+    },
+    {
+        id: 3,
+        title: 'Relationship between metacognition of motor, visual and memory processes',
+        authors: 'Arbuzova, P. Filevich, E.',
+        journal: 'Mind, Brain, Body Symposium, MindBrainBody Institute',
+        year: '2021',
+        link_journal: 'pdfs/MBB2021_arbuzova.pdf'
+    },
+    {
+        id: 2,
+        title: 'The relationship between visual and motor metacognition depends on the demands of the first-order task.',
+        authors: 'Arbuzova, P. Filevich, E.',
+        journal: 'ASSC 23, London, Ontario',
+        year: '2019',
+        link_journal: 'pdfs/ASSC23_Arbuzova_fixed_web.pdf'
+    }, {
+        id: 1,
+        title: 'Components of voluntary action',
+        authors: 'Arbuzova, P., Peters, C., Röd, L., Koß, C., Forster, C., Filevich, E. ',
+        journal: '9th Mind, Brain, Body Symposium, MindBrainBody Institute',
+        year: '2019',
+        link_journal: 'pdfs/Arbuzova_MBB_poster_Skittles_PDF.pdf'
+    }]
+
+
