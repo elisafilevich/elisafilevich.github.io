287af86f4a4b6d8049982c04758dd39949093fef
elisa.filevich <elisa.filevich@gmail.com>
2024-04-19 23:31:07 +0200

    removed google maps iframe because I would need to add a cookie banner. will look into this

--- components/footer.js
+++ components/footer.js
@@ -24,7 +24,7 @@ class Footer extends HTMLElement {
       </div><!-- /col-lg-4 -->
 
       <div class="col-lg-4">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2642.8237457919895!2d9.053511012671931!3d48.51744337116828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799fad16d1fc8c1%3A0x6cad6723574dcb05!2sEuropastra%C3%9Fe%206%2C%2072072%20T%C3%BCbingen!5e0!3m2!1sen!2sde!4v1682192701793!5m2!1sen!2sde" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2642.8237457919895!2d9.053511012671931!3d48.51744337116828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4799fad16d1fc8c1%3A0x6cad6723574dcb05!2sEuropastra%C3%9Fe%206%2C%2072072%20T%C3%BCbingen!5e0!3m2!1sen!2sde!4v1682192701793!5m2!1sen!2sde" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> -->
       </div><!-- /col-lg-4 -->
     </div>
     
