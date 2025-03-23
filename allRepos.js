60a44e846df248eca39f57b8c60f855a1835b3ff
elisa.filevich <elisa.filevich@gmail.com>
2025-03-23 20:56:40 +0100

    alternative, more boring, list to code repos. easier to maintain, no pictures. Will evaluate

---
+++ allRepos.js
@@ -0,0 +1,112 @@
+var allRepos = [
+    /* {
+        id: 0,
+        title: 'Dissociation of movement and outcome representations in metacognition of agency',
+        authors: 'Charalampaki, A., Maurer, H., Maurer, L.K. Muller, H. Filevich, E.',
+        year: '2024',
+        journal: 'BioRXiv',
+        link_repo: '',
+        link_pdf: 'pdfs_articles/Dissociation of movement and outcome representations in metacognition of agency.pdf'
+    }, */
+    {
+        id: 0,
+        title: 'Contributions of tactile information to the sense of agency and its metacognitive representations',
+        authors: 'Charalampaki, A., Ciston, A.B., Filevich, E.',
+        year: '2024',
+        journal: 'Journal of Experimental Psychology: General',
+        link_repo: 'https://osf.io/4tesr/',
+        link_pdf: 'pdfs_articles/Contributions of tactile information to the sense of agency and its metacognitive representations.pdf'
+    },
+    {
+        id: 29,
+        title: 'Prior information differentially affects discrimination decisions and subjective confidence reports',
+        authors: 'Constant, M., Pereira, M., Faivre, N., & Filevich, E.',
+        year: '2023',
+        journal: 'Nature Communications',
+        link_repo: 'https://gitlab.com/MarikaConstant/priors-in-confidence',
+        link_pdf: 'pdfs_articles/Prior information differentially affects discrimination decisions and subjective confidence reports.pdf'
+    }, {
+        id: 26,
+        title: 'Motor outcomes congruent with intentions may sharpen metacognitive representations',
+        authors: 'Charalampaki, A., Peters, C., Maurer, H., Maurer, LK., Müller, H., Verrel, J., Filevich, E.',
+        year: '2023',
+        journal: 'Cognition',
+        link_repo: 'https://gitlab.com/AngelikiC/metacognition-of-outcome-with-skittles',
+        link_pdf: 'pdfs_articles/Motor outcomes congruent with intentions may sharpen metacognitive representations.pdf'
+    },
+    {
+        id: 30,
+        title: 'Metacognitive domains are not aligned along a dimension of internal-external information source',
+        authors: 'Arbuzova, P., Maurer, L.K. & Filevich, E.',
+        year: '2022',
+        journal: 'Psychon Bull Rev',
+        link_repo: 'https://osf.io/bwkfp/',
+        link_pdf: 'pdfs_articles/Metacognitive domains are not aligned along a dimension of internal-external information source.pdf'
+    },
+    {
+        id: 27,
+        title: 'Do I look like I\'m sure?: Partial metacognitive access to the low-level aspects of one\'s own facial expressions',
+        authors: 'Ciston, AB.*, Forster, C.*, Brick, TR., Kühn, S., Verrel, J., Filevich, E.',
+        year: '2022',
+        journal: 'Cognition',
+        link_repo: 'https://gitlab.com/elisa.filevich/cistonetal_metacognitionoffacialexpressions',
+        link_pdf: 'pdfs_articles/Do I look like Im sure-Partial metacognitive access to the low-level aspects of ones own facial expressions.pdf'
+    },
+    {
+        id: 22,
+        title: 'No evidence for visual and tactile metacognition in adults with Tourette disorder',
+        authors: 'Arbuzova, P., Guo, S., Koß, C., Kurvis, L., Faivre, N., Kühn, A., Filevich, E., Ganos, C.',
+        journal: 'Parkinsonism & Related Disorders',
+        year: '2022',
+        link_repo: 'https://osf.io/3djxs/',
+        link_pdf: 'pdfs_articles/No evidence of impaired visual and tactile metacognition in adults with tourette disorder.pdf'
+    },
+    {
+        id: 21,
+        title: 'Judgments of agency are affected by sensory noise without recruiting metacognitive processing',
+        authors: 'Constant, M., Salomon, R., Filevich, E.',
+        journal: 'eLife',
+        year: '2022',
+        link_repo: 'https://gitlab.com/MarikaConstant/metaAgency',
+        link_pdf: 'pdfs_articles/Judgments of agency are affected by sensory noise without recruiting metacognitive processing.pdf'
+    },
+    {
+        id: 18,
+        title: 'Measuring metacognition of direct and indirect parameters of voluntary movement',
+        authors: 'Arbuzova, P., Peters, C., Roed, L., Koss, C., Maurer, H., Maurer, L. K., Mueller, H., Verrel, J., & Filevich, E.',
+        journal: 'Journal of Experimental Psychology: General',
+        year: '2021',
+        link_repo: 'https://gitlab.com/metamotor_lab/arbuzova.2021.meta-skittles',
+        link_pdf: 'pdfs_articles/Measuring Metacognition of Direct and Indirect Parameters of Voluntary Movement.pdf'
+    },
+    {
+        id: 17,
+        title: 'Response-related signals increase confidence but not metacognitive performance',
+        authors: 'Filevich, E., Koss, C. Faivre, N.',
+        journal: 'eNeuro',
+        year: '2020',
+        link_repo: 'https://gitlab.com/nfaivre/filevich_metareport/',
+        link_pdf: 'pdfs_articles/Response-Related Signals Increase Confidence But Not Metacognitive Performance.pdf'
+    },
+    {
+        id: 13,
+        title: 'Within-person adaptivity in frugal judgments from memory',
+        authors: 'Filevich, E.*, Horn, S.*, Kühn, S.',
+        journal: 'Psychological Research',
+        year: '2017',
+        link_repo: 'https://doi.org/10.5281/zenodo.888286',
+        link_pdf: 'pdfs_articles/Within-Person Adaptivity in Frugal Judgments from Memory.pdf'
+    }]
+
+
+// {id: 2,
+//     title: ,
+//     authors: ,
+//     journal: ,
+//     year: ,
+//     link_journal: ,
+//     link-pdf: ,
+//     link-supplement: ,
+//     link-preregistration
+// }
+
